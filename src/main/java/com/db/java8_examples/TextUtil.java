668244cbd2f25f6a2b3a8f39e42f798fffc73d56
Anastasiia Turenko <aaturenko@gmail.com>
2018-08-27 17:44:17 +0300

    update text util

--- src/main/java/com/db/java8_examples/TextUtil.java
+++ src/main/java/com/db/java8_examples/TextUtil.java
@@ -1,34 +1,40 @@
 package com.db.java8_examples;
 
-import lombok.SneakyThrows;
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 public class TextUtil {
 
+    public static Map<String, Integer> findWordsFrequency(BufferedReader reader){
+        return reader.lines()
+                .flatMap(line -> Arrays.stream(line.split("\\P{L}+")))
+                .collect(toMap(w -> w, w -> 1, (v1, v2) -> v1 + 1));
+    }
+
+    private static long findWordsAndCount(BufferedReader reader) {
+        List<String> lines = reader.lines().collect(Collectors.toList());
+        return lines.stream()
+                .flatMap(line -> Arrays.stream(line.split("\\P{L}+")))
+                .count();
+    }
+
+    private static double fingAvgLength(BufferedReader reader) {
+        return reader.lines()
+                .flatMap(line -> Arrays.stream(line.split("\\P{L}+")))
+                .mapToInt(String::length)
+                .average()
+                .orElse(0f);
+    }
 
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(TextUtil.class.getClassLoader().getResourceAsStream("text.txt")))) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            long countWords = lines.stream()
-                    .flatMap(line -> Arrays.stream(line.split("\\P{L}+")))
-                    .count();
-            System.out.println("There are " + countWords + " words");
-
-            double average = lines.stream()
-                    .flatMap(line -> Arrays.stream(line.split("\\P{L}+")))
-                    .mapToInt(String::length)
-                    .average()
-                    .orElse(0f);
-            System.out.println("The avg length is " + average);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(TextUtil.class.getClassLoader().getResourceAsStream("text.txt")));
+        Map<String, Integer> wordsFrequency = findWordsFrequency(reader);
+        System.out.println(wordsFrequency);
     }
 }
